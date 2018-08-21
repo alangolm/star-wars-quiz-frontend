651e88711675684631d7cf011c23590876dfab50
github username <github email address>
2018-08-21 11:08:46 -0400

    added my questions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const questions ={
+const questions = {
   question: "What year did Episode VI: Return of the Jedi release in North American theatres?",
   options: {
     a: "1980",
@@ -29,7 +29,7 @@ const questions ={
 },
 
 {
-  question: "How many parsecs did it take?",
+  question: "How many parsecs did Han Solo complete the Kessel Run in?",
   options: {
     a: "less than 12 parsecss",
     b: "About 14 parsecs",
@@ -97,3 +97,143 @@ const questions ={
   },
   correctAnswer: "a"
 },
+
+{
+  question: "'I think it is time we demonstrated the full power of this station.' Who said this?",
+  options: {
+    a: "Grand Moff Tarkin",
+    b: "The Emperor",
+    c: "Admiral Ackbar"
+  },
+  correctAnswer: "a"
+},
+
+{
+  question: "Who was Anakin Skywalker's father?",
+  options: {
+    a: "Cade Skywalker",
+    b: "Owen Lars",
+    c: "He didn't have one"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "What were the two opposing organizations in the Clone Wars?",
+  options: {
+    a: "The Galactic Republic and The Separatist Army",
+    b: "The Rebel Alliance and the Galactic Empire",
+    c: "The Gungan Grand Army and the Tusken Raiders"
+  },
+  correctAnswer: "a"
+},
+
+{
+  question: "Who cut off half of Anakin Skywalker’s right arm with a lightsaber?",
+  options: {
+    a: "Darth Maul",
+    b: "General Greivous",
+    c: "Count Dooku"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "What is Count Dooku’s Sith name?",
+  options: {
+    a: "Darth Tyranus",
+    b: "Darth Rex",
+    c: "Darth Sidious"
+  },
+  correctAnswer: "a"
+},
+
+{
+  question: "Name the alien race that incubated the clones used in the clone army?",
+  options: {
+    a: "Gungans",
+    b: "Genosians",
+    c: "Kaminoans"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "Who is the only non-Jedi/Sith character to use a lightsaber in the original trilogy?",
+  options: {
+    a: "Princess Leia",
+    b: "Han Solo",
+    c: "Boba Fett"
+  },
+  correctAnswer: "b"
+},
+
+{
+  question: "What game do Chewbacca and R2-D2 play in ‘A New Hope’?",
+  options: {
+    a: "Jugger",
+    b: "Cheops",
+    c: "Dejarik"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "What does the “TIE” in TIE Fighter stand for?",
+  options: {
+    a: "Turbo Induction Encabulator",
+    b: "Twin Ion Engine",
+    c: "Techno Incantho Envulcanator"
+  },
+  correctAnswer: "b"
+},
+
+{
+  question: "While filming ‘The Empire Strikes Back,’ which actor wasn’t sure if he/she wanted to return for a third movie?",
+  options: {
+    a: "Harrison Ford",
+    b: "Carrie Fisher",
+    c: "Mark Hamill"
+  },
+  correctAnswer: "a"
+},
+
+{
+  question: "What famous composer has scored all the ‘Star Wars’ films so far?",
+  options: {
+    a: "John Barry",
+    b: "Hans Zimmer",
+    c: "John Williams"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "Who was the first Jedi to learn how to return from death as a Force Ghost?",
+  options: {
+    a: "Yoda",
+    b: "Obi-Wan Kenobi",
+    c: "Qui-Gon Jinn"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "How old is Padme Amidala when she’s elected to the position of Queen of Naboo?",
+  options: {
+    a: "22",
+    b: "14",
+    c: "8"
+  },
+  correctAnswer: "b"
+},
+
+{
+  question: "Who plays Chewbacca?",
+  options: {
+    a: "Kenny Baker",
+    b: "David Prowse",
+    c: "Peter Mayhew"
+  },
+  correctAnswer: "c"
+}
